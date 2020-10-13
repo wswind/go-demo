e98743e9419b1a372c9cd818278fd7170e2c627a
ws <ws_dev@163.com>
2020-10-13 17:20:50 +0800

    up

--- goroutines01/goroutines01.go
+++ goroutines01/goroutines01.go
@@ -5,12 +5,39 @@ import (
 	"time"
 )
 
+// func main() {
+// 	go sayHello()
+// 	time.Sleep(100 * time.Millisecond)
+
+// }
+
+// func sayHello() {
+// 	fmt.Println("Hello")
+// }
+
+//----
+// func main() {
+// 	var msg = "Hello"
+// 	go func(msg string) {
+// 		fmt.Println(msg)
+// 	}(msg)
+
+// 	msg = "goodbye"
+
+// 	time.Sleep(100 * time.Millisecond)
+
+// }
+
+//----
+//go run -race goroutines01.go
 func main() {
-	go sayHello()
+	var msg = "Hello"
+	go func() {
+		fmt.Println(msg)
+	}()
+
+	msg = "goodbye"
+
 	time.Sleep(100 * time.Millisecond)
 
-}
-
-func sayHello() {
-	fmt.Println("Hello")
 }
